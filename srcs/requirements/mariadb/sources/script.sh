fdc3c4a2547b914fb0d8a0918d43d0c0fc4bd944
ahaddad98 <amine.haddadd@gmail.com>
2022-01-15 00:08:26 +0100

    redis done

--- srcs/requirements/mariadb/sources/script.sh
+++ srcs/requirements/mariadb/sources/script.sh
@@ -3,8 +3,8 @@ if !  ls var/lib/myqsl/sys &>/dev/null; then
 	
 /etc/init.d/mariadb setup
 /etc/init.d/mariadb start
-mysql -u root -e "CREATE DATABASE $DATABASE; CREATE USER '$DB_USER'@'%' IDENTIFIED BY 'DB_USER';GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"
-mysql -u root < $DATABASE.sql
+mysql -u root -e "CREATE DATABASE wordpress; CREATE USER 'ahaddad'@'%' IDENTIFIED BY 'ahaddad';GRANT ALL PRIVILEGES ON *.* TO 'ahaddad'@'%';"
+mysql -u root < wordpress.sql
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 /etc/init.d/mariadb stop
@@ -12,4 +12,3 @@ sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-ser
 fi
 
 exec /usr/bin/mysqld_safe
-

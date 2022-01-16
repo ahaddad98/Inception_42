caeed46221b217e7773650ddc86415c7717ac518
ahaddad98 <amine.haddadd@gmail.com>
2022-01-16 22:44:14 +0100

    ftp done

--- srcs/requirements/bonus/ftp/sources/script.sh
+++ srcs/requirements/bonus/ftp/sources/script.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+echo -e "123123123\n123123123" | adduser ahaddad
+echo ahaddad | tee -a /etc/vsftpd.userlist
+mkdir /home/ahaddad/wordpress
+chown -R ahaddad:ahaddad /home/ahaddad/
+#chmod 777 /home/ahaddad/wordpress
+
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
